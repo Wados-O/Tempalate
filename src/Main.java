7524135c5927acb73aff2abfc8485c3edc2caa95
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-08-01 21:49:41 +0200

    feat : add integers array

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.util.Random;
+
 public class Main {
 
   public static void main(String[] args) {
+    int[] integers = new int[10];
+    Random r = new Random();
+    for (int i = 0; i < integers.length; i++) {
+      int i1 = r.nextInt(10);
+      System.out.println(i1);
+
+    }
     System.out.println("Hello world!");
   }
 }
