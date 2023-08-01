857b197b747c3a5024830cd8556cbe6a9f0a84e3
Natalya Selyunina <seluninanv@mail.ru>
2023-08-01 23:22:43 +0300

    feat : add command

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,6 @@ public class Main {
     System.out.println("Wados");
     System.out.println("Dmitry");
     System.out.println("Natalya");
-    System.out.println("Team3");
+    System.out.println("Команда");
   }
 }
